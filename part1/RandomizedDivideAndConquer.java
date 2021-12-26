62a370d51f214ff6d4e6fff3da675f7af7be44cf
Youssef Victor <youssefvictor00@gmail.com>
2021-12-26 22:09:48 +0200

    Revert error

--- part1/RandomizedDivideAndConquer.java
+++ part1/RandomizedDivideAndConquer.java
@@ -1,6 +1,6 @@
 package part1;
 
-public class RandomizedDivideAndConquer implements MedianCalculatorr {
+public class RandomizedDivideAndConquer implements MedianCalculator {
     @Override
     public int getMedian(int[] array) {
         return 0;
