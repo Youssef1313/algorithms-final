ddf3c55e085a8501ef09a421ac14734286a112e1
mohamed-zayton <mzayton10@gmail.com>
2021-12-31 19:59:24 +0200

    random_median

--- part1/RandomizedDivideAndConquer.java
+++ part1/RandomizedDivideAndConquer.java
@@ -8,6 +8,50 @@ public class RandomizedDivideAndConquer implements MedianCalculator {
         }
 
         // TODO: Implement.
-        return 0;
+        int i = array.length / 2;
+        return medianRandomized(array, 0, array.length, i);
+    }
+
+    private int medianRandomized(int[] array, int p, int r, int i) {
+        if (p == r)
+            return array[p];
+        int q = randomizedPartition(array, p, r);
+
+        int k = q - p + 1;
+        if (i == k)
+            return array[q];
+        else if (i < k)
+            return medianRandomized(array, p, q-1, i);
+        else
+            return  medianRandomized(array, q+1, r, i-k);
+    }
+
+    private int randomizedPartition(int[] array, int p, int r) {
+        int i = (int) (Math.random() * p); //Get A Random Number Between p and r
+        int temp = array[i];
+        array[i] = array[r];
+        array[r] = temp;
+        return partition(array, p, r);
+    }
+
+    private int partition(int[] array, int p, int r) {
+        int x = array[p];
+        int i = p - 1;
+        int j = r + 1;
+        while (true){
+            while (array[j] <= x)
+                j-=1;
+            while (array[i] >= x)
+                i+=1;
+
+            if (i < j){
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }else
+                return j;
+
+        }
+
     }
 }
