f8a6780ebfa0c412434e8f69f0fac5ab5867db60
mohamed-zayton <mzayton10@gmail.com>
2022-01-06 19:08:56 +0200

    huffman

--- part2_6668/huffman/Huffman.java
+++ part2_6668/huffman/Huffman.java
@@ -1,36 +1,57 @@
 package huffman;
 
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Arrays;
+import java.util.*;
 
 
 public class Huffman {
-    private class Node{
-        Node (){
+    private class Node implements Comparable<Node> {
+        private final int freq;
+        private final Node left;
+        private final Node right;
+        private final BigInteger bytes;
 
+        // This constructor is used for non-leaf (inner) nodes.
+        public Node (Node left, Node right) {
+            this.left = left;
+            this.right = right;
+            this.freq = left.freq + right.freq;
+            this.bytes = null;
         }
-        Node(int f){
-            this.freq = f;
+
+        // This constructor is used for leaf nodes.
+        public Node(BigInteger bytes, int freq) {
+            this.freq = freq;
+            this.bytes = bytes;
+            this.left = null;
+            this.right = null;
         }
-        int freq=0;
-        Node left;
-        Node right;
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(freq, o.freq);
+        }
     }
     public void huffman(byte[] file_b, int number_n){
 
+        PriorityQueue<Node> pQueue = calculate_freq(file_b, number_n);
+        Node root = huffmanAlgorithm(pQueue);
+//        HashMap<BigInteger, Integer> huffmanDict = extractDict(root);
+
     }
 
+//    private HashMap<BigInteger, Integer> extractDict(Node root) {
+//
+//    }
+
     private PriorityQueue<Node> calculate_freq(byte[] file_b, int number_n){
-        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        HashMap<BigInteger,Integer> hashMap = new HashMap<>();
         PriorityQueue<Node> nodeQueue = new PriorityQueue<>();
         int prevIndex = 0;
         for (int i = 0; i < file_b.length; i+=number_n) {
             prevIndex = i;
             byte[] nByte = Arrays.copyOfRange(file_b, i,i+number_n);
-            int key = new BigInteger(nByte).intValue();
+            BigInteger key = new BigInteger(nByte);
             if (hashMap.containsValue(key)){
                 int v = hashMap.get(key);
                 hashMap.put(key, v+1);
@@ -38,12 +59,28 @@ public class Huffman {
                 hashMap.put(key, 1);
             }
         }
-        if (prevIndex != file_b.length) {
+        if (prevIndex+number_n != file_b.length) {
             //Store the remaining parts of the file
+            byte[] nByte = Arrays.copyOfRange(file_b, prevIndex, prevIndex+number_n);
+            byte[] arr = new byte[number_n];
+
+            for (int i = 0; i < number_n; i++) {
+                if (i<nByte.length)
+                    arr[i] = nByte[i];
+                else
+                    arr[i] = 0;
+            }
+            BigInteger key = new BigInteger(arr);
+            if (hashMap.containsValue(key)){
+                int v = hashMap.get(key);
+                hashMap.put(key, v+1);
+            }else{
+                hashMap.put(key, 1);
+            }
         }
 
-        for (Map.Entry<Integer, Integer> e :hashMap.entrySet()) {
-            Node node = new Node(e.getValue());
+        for (Map.Entry<BigInteger, Integer> e :hashMap.entrySet()) {
+            Node node = new Node(e.getKey(), e.getValue());
             nodeQueue.add(node);
         }
 
@@ -53,16 +90,15 @@ public class Huffman {
     private Node huffmanAlgorithm(PriorityQueue priorityQueue){
         int n = priorityQueue.size();
         PriorityQueue<Node> q = priorityQueue;
-
         for (int i = 0; i < n - 1; i++) {
-            Node node = new Node();
-            node.left = q.poll();
-            node.right = q.poll();
-            node.freq = node.left.freq + node.right.freq;
+            var left = q.poll();
+            var right = q.poll();
+            Node node = new Node(left, right);
+
             q.add(node);
         }
-
         return q.poll();
     }
+
 }
 
--- part2_6668/huffman/Main.java
+++ part2_6668/huffman/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Compress compress = new Compress();
-        compress.readingFile("GA.pdf");
+        compress.readingFile("/media/zayton/HDD-Data/desktop/eng/7thTerm/Alg/algorithms-final/part2_6668/GA.pdf");
         System.out.printf("ok");
     }
 }
