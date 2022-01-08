6b4ba009f6d25e5741258efe89aad0146ac26686
mohamed-zayton <mzayton10@gmail.com>
2022-01-08 17:36:15 +0200

    fix

--- part2_6668/huffman/Main.java
+++ part2_6668/huffman/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Compress compress = new Compress();
         byte[] fb = compress.readingFile("/media/zayton/HDD-Data/desktop/eng/7thTerm/Alg/algorithms-final/part2_6668/file");
         Huffman huffman = new Huffman();
-        huffman.huffman(fb, 1);
-        System.out.printf("ok");
+        huffman.huffman(fb, 2);
+        System.out.println("ok");
     }
 }
